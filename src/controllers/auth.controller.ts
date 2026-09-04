5cbf6d7c6934c4fa9d7f0abbee746a05620bf6aa
Pawan Hada <hadapawan13@gmail.com>
2026-09-04 09:23:36 +0545

    line fix

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -182,6 +182,7 @@ export const updateProfile = catchAsync(async (req: Request, res: Response) => {
 // change password
 export const changePassword = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
+    //const {_id,email}=req.user
     const { email, password, new_password } = req.body;
 
     if (!email) throw new AppError("email is required", 400);
